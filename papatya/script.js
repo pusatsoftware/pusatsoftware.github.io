98a94b84c22b92265ff34263b0ce9c8c909d3668
Pusat Software <emirhanpusat0@gmail.com>
2025-06-04 00:20:10 +0300

    Update script.js

--- papatya/script.js
+++ papatya/script.js
@@ -76,7 +76,7 @@ class Flower {
       ctx.moveTo(0, 0);
       ctx.bezierCurveTo(2 * this.scale, -5 * this.scale, 3 * this.scale, -12 * this.scale, 0, -14 * this.scale);
       ctx.bezierCurveTo(-3 * this.scale, -12 * this.scale, -2 * this.scale, -5 * this.scale, 0, 0);
-      ctx.fillStyle = "black";
+      ctx.fillStyle = "white";
       ctx.shadowColor = "rgba(200,200,200,0.6)";
       ctx.shadowBlur = 5;
       ctx.fill();
@@ -113,7 +113,6 @@ function spawnBranch(x, y, depth, angle, length) {
 }
 
 function animate() {
-  ctx.fillStyle = "#000";
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   for (let b of branches) {
